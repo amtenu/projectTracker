0c16b9e3e8102d1eab46c5dc679c94ba9b230937
amtenu <amannov21@gmail.com>
2024-04-25 14:50:08 -0600

    Initial setup

---
+++ backend/src/test/java/com/amtenu/ProjecttrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amtenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjecttrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
