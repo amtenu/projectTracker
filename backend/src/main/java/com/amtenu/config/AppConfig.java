0229c2bd35bf07da43be142b60b24d8e5383bbe8
amtenu <amannov21@gmail.com>
2024-04-26 12:26:35 -0600

    Implimented jwt logic with headers and requests with token

--- backend/src/main/java/com/amtenu/config/AppConfig.java
+++ backend/src/main/java/com/amtenu/config/AppConfig.java
@@ -55,6 +55,7 @@ public class AppConfig {
     }
     @Bean
     PasswordEncoder passwordEncoder(){
+
         return new BCryptPasswordEncoder();
     }
 
---
+++ backend/src/main/java/com/amtenu/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.amtenu.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY="Eritrea is a beautiful East African 1991";
+    public  static final String JWT_HEADER="Authorization";
+}
