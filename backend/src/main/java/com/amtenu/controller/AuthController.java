48f7935ec5fc068c30f5d763215af6d28f03c412
amtenu <amannov21@gmail.com>
2024-04-27 18:42:48 -0600

    users can login to app and token and success message is returned

--- backend/src/main/java/com/amtenu/controller/AuthController.java
+++ backend/src/main/java/com/amtenu/controller/AuthController.java
@@ -4,15 +4,18 @@ package com.amtenu.controller;
 import com.amtenu.config.JwtProvider;
 import com.amtenu.models.User;
 import com.amtenu.repository.UserRepository;
+import com.amtenu.request.LoginRequest;
 import com.amtenu.response.AuthResponse;
 import com.amtenu.service.CustomUserDetailImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.CachingUserDetailsService;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -55,6 +58,34 @@ public class AuthController {
         res.setJwt(jwt);
 
         return new ResponseEntity<>(res, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<AuthResponse> signin (@RequestBody LoginRequest loginRequest){
+        String username=loginRequest.getEmail();
+        String password=loginRequest.getPassword();
+
+        Authentication authentication=autenticate(username,password);
+        String jwt= JwtProvider.generateToken(authentication);
+
+        AuthResponse res=new AuthResponse();
+        res.setMessage("Signin success");
+        res.setJwt(jwt);
+
+        return new ResponseEntity<>(res, HttpStatus.CREATED);
+
+    }
+
+    private Authentication autenticate(String username, String password) {
+        UserDetails userDetails=customUserDetails.loadUserByUsername(username);
+        if(userDetails==null){
+            throw new BadCredentialsException("Invalid Username or password");
+        }
+        if(!passwordEncoder.matches(password,userDetails.getPassword())){
+            throw new BadCredentialsException("Invalid Username or password");
+
+        }
+        return new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
     }
 
 }
