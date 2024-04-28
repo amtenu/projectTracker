215db7b0a0c0bdc2b379a6e40ee65ac6088b480c
amtenu <amannov21@gmail.com>
2024-04-28 11:29:17 -0600

    Created Rest API controllers for getting all projects and get projects by ID

---
+++ backend/src/main/java/com/amtenu/controller/ProjectController.java
@@ -0,0 +1,47 @@
+package com.amtenu.controller;
+
+
+import com.amtenu.models.Project;
+import com.amtenu.models.User;
+import com.amtenu.service.ProjectService;
+import com.amtenu.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/projects")
+public class ProjectController {
+
+    @Autowired
+    private ProjectService projectService;
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<Project>> getProjects(
+            @RequestParam(required = false) String category,
+            @RequestParam(required = false) String tag,
+            @RequestHeader("Authorization") String jwt) throws Exception {
+        User user = userService.findUserProfileByJwt(jwt);
+        List<Project> projects = projectService.getProjectByTeam(user, category, tag);
+        return new ResponseEntity<>(projects, HttpStatus.OK);
+
+
+    }
+
+    @GetMapping("/{projectId}")
+    public ResponseEntity<Project> getProjectByID(
+            @PathVariable Long projectId,
+            @RequestHeader("Authorization") String jwt) throws Exception {
+        User user = userService.findUserProfileByJwt(jwt);
+        Project project = projectService.getProjectById(projectId);
+        return new ResponseEntity<>(project, HttpStatus.OK);
+
+
+    }
+}
