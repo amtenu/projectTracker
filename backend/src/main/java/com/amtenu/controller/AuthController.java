60e7cb7f735a26e0ea5f9811ebaf4f10db7a6e07
amtenu <amannov21@gmail.com>
2024-04-27 18:28:35 -0600

    Spring returns token and message instade of user details and password

--- backend/src/main/java/com/amtenu/controller/AuthController.java
+++ backend/src/main/java/com/amtenu/controller/AuthController.java
@@ -1,14 +1,18 @@
 package com.amtenu.controller;
 
 
+import com.amtenu.config.JwtProvider;
 import com.amtenu.models.User;
 import com.amtenu.repository.UserRepository;
+import com.amtenu.response.AuthResponse;
 import com.amtenu.service.CustomUserDetailImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.CachingUserDetailsService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,7 +34,7 @@ public class AuthController {
     private CustomUserDetailImpl customUserDetails;
 
     @PostMapping("/signup")
-    public ResponseEntity<User>createdUserHandler(@RequestBody User user) throws Exception {
+    public ResponseEntity<AuthResponse>createdUserHandler(@RequestBody User user) throws Exception {
         User userExists = userRepository.findByEmail(user.getEmail());
         if (userExists != null) {
             throw new Exception("Email is already exists ");
@@ -43,7 +47,14 @@ public class AuthController {
 
         User savedUser=userRepository.save(createdUser);
 
-        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+        Authentication authentication=new UsernamePasswordAuthenticationToken(user.getEmail(),user.getPassword());
+        String jwt= JwtProvider.generateToken(authentication);
+
+        AuthResponse res=new AuthResponse();
+        res.setMessage("Signup success");
+        res.setJwt(jwt);
+
+        return new ResponseEntity<>(res, HttpStatus.CREATED);
     }
 
 }
