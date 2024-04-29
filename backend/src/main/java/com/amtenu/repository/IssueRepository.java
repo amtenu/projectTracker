32f646f7121001410d267dac39f98bd63366974a
amtenu <amannov21@gmail.com>
2024-04-29 15:19:39 -0600

    Created Issue serice, Issue Repository and Issue implimentation methods

---
+++ backend/src/main/java/com/amtenu/repository/IssueRepository.java
@@ -0,0 +1,12 @@
+package com.amtenu.repository;
+
+import com.amtenu.models.Issue;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface IssueRepository extends JpaRepository<Issue, Long> {
+
+    public List<Issue> findByProjectId(Long id);
+
+}
---
+++ backend/src/main/java/com/amtenu/service/IssueService.java
@@ -0,0 +1,24 @@
+package com.amtenu.service;
+
+import com.amtenu.models.Issue;
+import com.amtenu.models.User;
+import com.amtenu.request.IssueRequest;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IssueService {
+    Issue getIssueByID(Long issueId) throws Exception;
+
+    List<Issue> getIssuesByProjectId(Long projectId) throws Exception;
+
+    Issue createNewIssue(IssueRequest issue, User user) throws Exception;
+
+  void deleteIssue(Long issueId) throws Exception;
+
+   Issue addUserToIssue(Long issueId,Long userId) throws Exception;
+
+   Issue updateStatus(Long issueId,String status) throws Exception;
+
+
+}
