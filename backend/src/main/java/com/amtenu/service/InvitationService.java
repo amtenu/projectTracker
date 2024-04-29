a488fca806fc7d48b673912b6820f3c417a185a4
amtenu <amannov21@gmail.com>
2024-04-29 14:05:15 -0600

    Created accept invitation, get token by userEmail and deleting invitation

--- backend/src/main/java/com/amtenu/service/InvitationService.java
+++ backend/src/main/java/com/amtenu/service/InvitationService.java
@@ -6,7 +6,7 @@ import jakarta.mail.MessagingException;
 public interface InvitationService {
 
     public void sendInvitation(String email,Long projectId) throws MessagingException;
-    public Invitation acceptInvitation(String token, Long userId);
+    public Invitation acceptInvitation(String token, Long userId) throws Exception;
 
     public String getTokenByUserMail(String userEmail);
 
