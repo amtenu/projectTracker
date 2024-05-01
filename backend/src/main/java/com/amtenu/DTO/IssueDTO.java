4e07d45ca5311db41e6809f0caed822f3e110a9a
amtenu <amannov21@gmail.com>
2024-04-30 23:21:37 -0600

    Created a data transfer object for issue class

---
+++ backend/src/main/java/com/amtenu/DTO/IssueDTO.java
@@ -0,0 +1,30 @@
+package com.amtenu.DTO;
+
+import com.amtenu.models.Project;
+import com.amtenu.models.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class IssueDTO {
+
+    private Long Id;
+    private String title;
+    private String description;
+    private String status;
+    private String projectId;
+
+    private String priority;
+    private String dueDate;
+    private List<String> tags=new ArrayList<>();
+    private Project project;
+
+    private User assignee;
+
+}
